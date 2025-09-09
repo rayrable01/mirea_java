042d084ad313f4e3d459cfd9e91144ad4014fcc6
Daniil Gnatiuk <gnatiukdanya@gmail.com>
2025-09-09 16:27:15 +0300

    remove: deleted data from task2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
        System.out.println("Начало выполнения задания...");
-
-       Author author = new Author("Danya", "test@test.ru", 'M');
-
-       System.out.println(author.toString());
     }
 }
