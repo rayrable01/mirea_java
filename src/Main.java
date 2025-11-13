e9bfd29fdf9898cced3f29381d581f721089926b
Daniil Gnatiuk <gnatiukdanya@gmail.com>
2025-11-13 18:33:49 +0300

    feat: pracitce 4 task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Начало выполнения заданий...");
+        Person person = new Person();
+        person.iLove(Seasons.AUTUMN);
+
+        for (Seasons season : Seasons.values()) {
+            System.out.println(season.getSeasonInfo());
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,10 @@
 public class Person {
-    private Seasons favoriteSeason = Seasons.SPRING;
-
-    public void iLove(Seasons season) {
-        switch (season) {
+    public void iLove(Seasons favoriteSeason) {
+        switch (favoriteSeason) {
             case AUTUMN -> System.out.println("Я не люблю осень");
             case SPRING -> System.out.println("Я чутка люблю весну");
             case SUMMER -> System.out.println("Я очень люблю лето");
             case WINTER -> System.out.println("Я не люблю зиму");
-        };
+        }
     }
 }
