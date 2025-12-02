6c7ee6de6a66b2c2245fbe9898c26094cfb1463b
Daniil Gnatiuk <gnatiukdanya@gmail.com>
2025-12-02 16:50:45 +0300

    remove: files from practice 4

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // 4) Создаем массив, содержащий все типы одежды
-        Clothes[] wardrobe = {
-                new TShirt(Sizes.M, 1500.0, "Белый"),
-                new TShirt(Sizes.S, 1200.0, "Черный"),
-                new Pants(Sizes.L, 3000.0, "Синий"),
-                new Pants(Sizes.M, 2500.0, "Черный"),
-                new Skirt(Sizes.S, 2000.0, "Красный", "Карандаш"),
-                new Skirt(Sizes.XS, 1800.0, "Розовый", "Солнце"),
-        };
-
-        // Создаем ателье
-        Atelier atelier = new Atelier();
-
-        System.out.println("\n" + "=".repeat(50) + "\n");
-
-        // Альтернативный вывод - только информация
-        atelier.displayWomenClothing(wardrobe);
-        System.out.println();
-        atelier.displayMenClothing(wardrobe);
+        System.out.println("Hello");
     }
 }
--- src/WomenClothing.java
+++
@@ -1,3 +0,0 @@
-public interface WomenClothing {
-    void dressWoman();
-}
