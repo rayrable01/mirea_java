ef972d8d1fc663f7f5b7bdb2a252dcd97dd7789e
Daniil Gnatiuk <gnatiukdanya@gmail.com>
2025-12-11 16:24:50 +0300

    feat: completed Practice 11

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            AppWindow window = new AppWindow();
-            window.setVisible(true);
-        });
+
+        System.out.println("Задание 7: Разложение на множители");
+        Factorization.factor(360);
+        System.out.println("\n");
+
+        System.out.println("Задание 8: Палиндром");
+        System.out.println(Palindrome.isPalindrome("madam") ? "YES" : "NO");
+        System.out.println(Palindrome.isPalindrome("hello") ? "YES" : "NO");
+        System.out.println();
+
+        System.out.println("Задание 9: Без двух нулей");
+        System.out.println(NoTwoZeros.count(2, 3));
+        System.out.println(NoTwoZeros.count(3, 3));
+        System.out.println();
+
+        System.out.println("Задание 10: Разворот числа");
+        System.out.println(ReverseNumber.reverse(12345));
+        System.out.println(ReverseNumber.reverse(987654321));
     }
 }
